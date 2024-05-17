50294d6f350772ae5701d99b014a5978c1116587
Jason Alvarado <jayxalvarado@gmail.com>
2024-05-17 00:54:14 -0700

    cleanup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,10 @@ import hero from "../assets/hero.jpg";
 
 //img full width of container, responsive regardless of width. max height at 600px. object cover keep aspect ratio to avoid distortion
 const Hero = () => {
+  //hero image contain. Might change to cover
   return (
     <div>
-      <img src={hero} className="w-full max-h-[600px] object-cover" />
+      <img src={hero} className="w-full max-h-[600px] object-contain" />
     </div>
   );
 };
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 //rfcp prop props shortcut
 type Props = {
@@ -10,11 +11,13 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     //takes up entire width of screen
+    //since parent div has height of screen, footer will be at bottom of screen
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
 
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 };
