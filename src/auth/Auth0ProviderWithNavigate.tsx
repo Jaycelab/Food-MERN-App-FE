bf9289470ea427ff8d6b839351f4c591af9b0089
Jason Alvarado <jayxalvarado@gmail.com>
2024-05-18 04:23:01 -0700

    Initial setup for Auth0

---
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,40 @@
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+
+type Props = {
+  //children is a special property that is passed to components automatically
+  children: React.ReactNode;
+};
+
+//wrap all components to access Auth0Provider
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  //storing Auth0 domain in environment variable
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  //check if domain, clientId, and redirectUri are defined
+  if (!domain || !clientId || !redirectUri) {
+    throw new Error(
+      "Please define the Auth0 domain, clientId, and redirectUri in your .env file"
+    );
+  }
+
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    console.log("USER", user);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      //whenver user logs in, they will be redirected to the home page {redirectUri}.
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+export default Auth0ProviderWithNavigate;
--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,11 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 //variant ghost removes styles to manually style. hover prop written twice for multiple effects
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Button
       variant="ghost"
       className="font-bold hover:text-orange-500 hover:bg-white"
+      onClick={async () => loginWithRedirect()}
     >
       Log In
     </Button>
